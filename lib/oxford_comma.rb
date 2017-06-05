e3f5f904ce5804154f3753a0528a1ec07b02c4e7
Jevaughn McKenzie <jmckenzie1004@gmail.com>
2017-06-05 20:07:59 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
 
+  if (array.count > 2)
+    popped_element = array.pop
+    stringed_array = array.join(", ")
+
+    stringed_array << ", and "
+    stringed_array << popped_element
+  elsif (array.count > 1)
+    array.join(" and ")
+  else
+    array.join
+  end
+
+
 end
+
+# remove the last word
+# convert array to string
+# shovel ' and ' and the popped element
